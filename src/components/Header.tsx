b64fd3b408fcc94279105706bb7c069594847aec
Luiz Felipe Warmling Amadeu <85834483+Amad3eu@users.noreply.github.com>
2022-04-20 23:52:46 -0300

    add-principal-files

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+ 
+interface props {
+  title: string
+}
+
+const Header: React.FC<props> = ({ title }) => {
+  return (
+    <header className="bg-white shadow">
+      <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <h1 className="text-3xl font-bold leading-tight text-gray-900">
+          {title}
+        </h1>
+      </div>
+    </header>
+  )
+}
+export default Header
---
+++ src/views/NotFound.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NotFound = () => {
+  return <main>Not Found</main>
+};
+
+export default NotFound
